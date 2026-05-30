2c3a0617dc659eeed0ce7f35d464c38a560d6e48
dudco <dudco1129@naver.com>
2026-05-31 01:24:47 +0900

    admin 비밀번호 6자 이하도 가능하도록 수정

--- scripts/create-admin-hash.ts
+++ scripts/create-admin-hash.ts
@@ -2,12 +2,12 @@ import bcrypt from "bcryptjs";
 
 const password = process.argv[2];
 
-if (!password || password.length < 12) {
+if (!password || password.length < 6) {
   console.error(
-    "Usage: pnpm dlx tsx scripts/create-admin-hash.ts '<password-with-at-least-12-chars>'"
+    "Usage: pnpm dlx tsx scripts/create-admin-hash.ts '<password-with-at-least-6-chars>'"
   );
   process.exit(1);
 }
 
-const hash = await bcrypt.hash(password, 12);
+const hash = await bcrypt.hash(password, 6);
 console.log(hash);
